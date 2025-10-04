5477836889e5d605d8cd842351ac88e541734094
Jordan kirby <jkirbyprogrammer@gmail.com>
2025-10-03 19:50:00 -0500

    Added project files.

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+  import Dashboard from './components/Dashboard.vue'
+  import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+
+  <header>
+    <div>
+
+      <div class="centered-container">
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="50" height="50" />
+      </div>
+      <div class="centered-container">
+        <div>
+          <h3>Interactive map using crop, disaster designation, and USFS fire occurance data from USDA at the state/county level.</h3>
+        </div>
+      </div>
+      <div class="centered-container">
+        <div>
+          <p>TO DO: Add president and ussec buttons here to toggle map data.</p>
+          <p>TO DO: Add year options here to toggle map data here...</p>
+        </div>
+      </div>
+      <div>
+        <Dashboard />
+      </div>
+      <div>
+        <div class="left-list">
+          <ul>
+            <li>Use the layer option to toggle between state/county level details or change the base layer.</li>
+            <li>Use Presidential Emergency Declarations/US Secretary of Ag buttons to toggle data in map.</li>
+            <li>Click on the states/counties to see details. <b>Note:</b> Defaults to county as the primary layer, use layer option to toggle.</li>
+            <li>Use the year links to update the map year data set.</li>
+            <li>Points represent ignition or origin from which the individual USFS wildland fires started by year. <b>Note:</b> Until full dataset is out for year 2025, the 2024 data will show when 2025 year is selected. </li>
+            <li>
+              Data sources used to create GeoJson for map:
+              <ul>
+                <li>
+                  <a target="_blank" href="https://www.fsa.usda.gov/resources/disaster-assistance-program/disaster-designation-information">
+                    USDA Disaster Designation Information
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://quickstats.nass.usda.gov/">
+                    USDA Quick Stats Census
+                  </a>
+                </li>
+                <li>
+                  <a target="_blank" href="https://catalog.data.gov/dataset/national-usfs-fire-occurrence-point-feature-layer-d3233">
+                    National USFS Fire Occurrence Point
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <!--<TheWelcome />-->
+  </header>
+</template>
+
+<style scoped>
+  header {
+    line-height: 1.5;
+  }
+
+  .logo {
+    display: block;
+    margin: 0 auto 2rem;
+  }
+
+  @media (min-width: 1024px) {
+    header {
+      display: flex;
+      place-items: center;
+      padding-right: 0;
+    }
+
+    .logo {
+      margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+      display: flex;
+      place-items: center;
+      flex-wrap: wrap;
+    }
+  }
+</style>
+
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="map-container" >
+    <l-map :useGlobalLeaflet="false" ref="map" v-model:zoom="zoom" :center="[37.8, -96]">
+      <l-tile-layer url="https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}'"
+                    layer-type="base"
+                    name="World Light Grey Base">
+      </l-tile-layer>
+      <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    layer-type="base"
+                    name="Open Street Map Base">
+      </l-tile-layer>
+      <l-tile-layer url="https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'"
+                    layer-type="base"
+                    name="World Imagery Base">
+      </l-tile-layer>
+      <l-layer-group ref="geoJsonLayer" layer-type="overlay" name="State Layer">
+        <l-geo-json :geojson="geojsonstate" :options-style="geoStateStyler" :options="geojsonStateOptions" />
+      </l-layer-group>
+      <l-layer-group ref="geoJsonLayer2" layer-type="overlay" name="County Layer">
+        <l-geo-json :geojson="geojsoncounty" :options-style="geoCountyStyler" :options="geojsonCountyOptions"></l-geo-json>
+      </l-layer-group>
+      <l-layer-group ref="geoJsonLayer3" layer-type="overlay" name="Fire Points Layer">
+        <l-geo-json :geojson="geojsonfire" :options="geojsonFireOptions"></l-geo-json>
+      </l-layer-group>
+      <l-control-layers />
+    </l-map>
+  </div>
+</template>
+
+<script>
+
+  import "leaflet/dist/leaflet.css"
+  import { LGeoJson, LMap, LTileLayer, LPopup, LControlLayers, LLayerGroup } from "@vue-leaflet/vue-leaflet";
+  const { circleMarker } = await import("leaflet/dist/leaflet-src.esm");
+
+  export default {
+    components: {
+      LMap,
+      LTileLayer,
+      LGeoJson,
+      LPopup,
+      LControlLayers,
+      LLayerGroup
+    },
+    methods: {
+      onEachStateFeature(feature, layer) {
+        if (feature.properties) {
+          var props = feature.properties;
+          layer.bindPopup(`
+        <div>
+          <h3>${props.name}</h3>
+          <div><b>Total Emergency Declarations: </b>${props.TotalPresDecs}</div>
+          <div><b>Declared Disasters: </b>${props.ListOfDisasters}</div>
+        </div>`);
+        }
+        else {
+          layer.bindPopup('No Feature Properties yet')
+        }
+      },
+      onEachCountyeature(feature, layer) {
+        if (feature.properties) {
+          var props = feature.properties;
+          layer.bindPopup(`
+        <div>
+          <h3>${props.name}</h3>
+          <div><b>Total Emergency Declarations: </b>${props.TotalPresDecs}</div>
+          <div><b>Declared Disasters: </b>${props.ListOfDisasters}</div>
+          <div><b>Total crop production types: </b>${props.DecsWithCrops}</div>
+          <div><b>Crop Details: </b>${props.CropDetailList}</div>
+        </div>`);
+        }
+        else {
+          layer.bindPopup('No Feature Properties yet')
+        }
+      }
+    },
+    data() {
+      return {
+        zoom: 4,
+        geojsonstate: undefined,
+        geojsonfire: undefined,
+        geojsoncounty: undefined,
+        geoStateStyler: (feature) => ({
+          fillColor: getColor(feature.properties.TotalPresDecs),
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          fillOpacity: 0.7
+        }),
+        geoCountyStyler: (feature) => ({
+          fillColor: getColorCounty(feature.properties.TotalPresDecs, feature.properties.DecsWithCrops),
+          weight: .6,
+          opacity: .5,
+          color: "black",
+          fillOpacity: 0.7
+        }),
+        geojsonStateOptions: {
+          onEachFeature: this.onEachStateFeature,
+        },
+        geojsonCountyOptions: {
+          onEachFeature: this.onEachCountyeature,
+        },
+        geojsonFireOptions: {
+          pointToLayer: (feature, latlng) => {
+            return circleMarker(latlng, {
+              radius: 7,
+              fillColor: feature.properties ? "#FDA50F" : "red",
+              color: "#000",
+              weight: 1,
+              opacity: 1,
+              fillOpacity: 0.8
+            });
+          },
+          onEachFeature(feature, layer) {
+            if (feature.properties) {
+              var props = feature.properties;
+              layer.bindPopup(`
+            <div>
+              <h3>${props.FIRENAME}</h3>
+              <div><b>Fire Year: </b>${props.FIREYEAR}</div>
+              <div><b>Fire Discovery Date/Time: </b>${props.DISCOVERYDATETIME}</div>
+            </div>`);
+            }
+            else {
+              layer.bindPopup('No Feature Properties yet')
+            }
+          }
+        }
+      };
+    },
+    async beforeMount() {
+      this.geojsonstate = await GetGeoJson("src/GeoJson/2025StateUsSecLayer.json");
+      this.geojsoncounty = await GetGeoJson("src/GeoJson/2025CountyUsSecLayer.json");
+      this.geojsonfire = await GetGeoJson("src/GeoJson/2024NationalUSFSFireOccurrencePoint.json");
+
+      this.mapIsReady = true;
+    },
+
+  };
+
+  const currentInterval = 5;
+  function getColor(d) {
+    return d >= (4 * currentInterval) ? '#011a08' :
+      d > (3 * currentInterval) ? '#3d9137' :
+        d > (2 * currentInterval) ? '#88d669' :
+          (d > (1 * currentInterval) || d > 0) ? '#b2ed9a' :
+            '#FFFFFF00';
+  }
+
+  function getColorCounty(totalDecs, totalDecsWithCrops) {
+    return totalDecs > 0 && totalDecsWithCrops > 0 ? 'red' :
+      totalDecs > 0 ? '#5E87E8' :
+        '#FFFFFF00';
+  }
+
+  async function GetGeoJson(path) {
+    const response = await fetch(path);
+    const data = await response.json();
+    return data;
+  }
+
+
+</script>
